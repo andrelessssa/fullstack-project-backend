240c656dd3ef7d68d6192e7fa467768c0192cd10
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-10-07 20:56:42 -0300

    Initial project structure from Spring Initializr
    Initial project structure from Spring Initializr

---
+++ backend/src/main/java/br/com/andrelessa/fullstack_project_backend/FullstackProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.andrelessa.fullstack_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackProjectBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/andrelessa/fullstack_project_backend/FullstackProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrelessa.fullstack_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
